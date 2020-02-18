69c6892906dd53434b321cfacdefe1efc2871ea6
Alexander Victor <alxv.oliveira@gmail.com>
2020-02-17 21:06:57 -0300

    05.Funções que auxiliam os estilos

--- public/js/main.js
+++ public/js/main.js
@@ -1,25 +1,26 @@
-var tempoInicial = $("#tempo-digitacao").text();
 var campo = $(".campo-digitacao");
+var tempoInicial = $("#tempo-digitacao").text();
 
 $(function(){
     atualizaTamanhoFrase();
     inicializaContadores();
     inicializaCronometro();
+    inicializaMarcadores();
     $("#botao-reiniciar").click(reiniciaJogo);
 });
 
-function atualizaTamanhoFrase(){
+function atualizaTamanhoFrase() {
     var frase = $(".frase").text();
-    var numPalavras  = frase.split(" ").length;
+    var numPalavras = frase.split(" ").length;
     var tamanhoFrase = $("#tamanho-frase");
     tamanhoFrase.text(numPalavras);
 }
 
 function inicializaContadores() {
-    campo.on("input",function() {
+    campo.on("input", function() {
         var conteudo = campo.val();
 
-        var qtdPalavras = conteudo.split(" ").length;
+        var qtdPalavras = conteudo.split(/\S+/).length - 1;
         $("#contador-palavras").text(qtdPalavras);
 
         var qtdCaracteres = conteudo.length;
@@ -30,24 +31,43 @@ function inicializaContadores() {
 function inicializaCronometro() {
     var tempoRestante = $("#tempo-digitacao").text();
     campo.one("focus", function() {
-        $("#botao-reiniciar").attr("disabled", true);
         var cronometroID = setInterval(function() {
             tempoRestante--;
             $("#tempo-digitacao").text(tempoRestante);
             if (tempoRestante < 1) {
                 campo.attr("disabled", true);
                 clearInterval(cronometroID);
-                $("#botao-reiniciar").attr("disabled", false);
+                campo.toggleClass("campo-desativado");
             }
         }, 1000);
     });
-}   
+}
 
-function reiniciaJogo(){
-    campo.attr("disabled",false);
+function inicializaMarcadores() {
+    var frase = $(".frase").text();
+    campo.on("input", function() {
+        var digitado = campo.val();
+        var comparavel = frase.substr(0 , digitado.length);
+
+        if(digitado == comparavel) {
+            campo.addClass("borda-verde");
+            campo.removeClass("borda-vermelha");
+        } else {
+            campo.addClass("borda-vermelha");
+            campo.removeClass("borda-verde");
+        }
+    });
+}
+
+function reiniciaJogo() {
+    campo.attr("disabled", false);
     campo.val("");
     $("#contador-palavras").text("0");
     $("#contador-caracteres").text("0");
     $("#tempo-digitacao").text(tempoInicial);
+
     inicializaCronometro();
-}
+    campo.toggleClass("campo-desativado");
+    campo.removeClass("borda-vermelha");
+    campo.removeClass("borda-verde");
+};
